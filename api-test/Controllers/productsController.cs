49b8825f03291a07ec9a76bc787fa1d146032529
abdullah <abdullah2309a@aptechorangi.com>
2025-03-28 21:14:48 -0400

    DELETE: api/products/5

--- api-test/Controllers/productsController.cs
+++ api-test/Controllers/productsController.cs
@@ -84,6 +84,7 @@ namespace api_test.Controllers
         }
 
         // DELETE: api/products/5
+        // DELETE: api/products/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> Deleteproduct(int id)
         {
