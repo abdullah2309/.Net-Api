a8c815b70ed5c110639d3556b1e6c0eebbc5835a
abdullah <abdullah2309a@aptechorangi.com>
2025-09-03 12:46:38 -0400

    update user.cs

--- api-test/Model/User.cs
+++ api-test/Model/User.cs
@@ -1,6 +1,25 @@
-﻿namespace api_test.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_test.Model
 {
     public class User
     {
+        [Key] // Primary key
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string PasswordHash { get; set; } // store hashed password, not plain text
+
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
